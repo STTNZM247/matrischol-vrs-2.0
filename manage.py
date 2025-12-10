ec2494305fbcfe1cab7f5ed9d548061d1c0a9d81
STTNZM247 <sttnzm247@example.com>
2025-12-10 14:31:14 -0500

    configuracion de setting13

--- manage.py
+++ manage.py
@@ -3,6 +3,14 @@ import os
 import sys
 
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME', ''),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY', ''),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET', ''),
+}
+
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matrischol_project.settings')
     try:
